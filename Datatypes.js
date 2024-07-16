8cfb53451dc6987ee05a0cf5066f56a5ae9d1feb
Premchand Hansda <premchandhansda006@gmail.com>
2024-07-16 21:22:54 +0000

    Create Datatypesjs

---
+++ Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Premchand Hansda")
+
+
+let name = "Premchand Hansda"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+console.log(typeof name);  //string
+console.log(typeof age);  //number
+console.log(typeof isLoggedIn);  //boolean
+console.log(typeof state);  //undefined
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
