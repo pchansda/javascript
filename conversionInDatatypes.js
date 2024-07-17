1a5faa96ff256a85796b4f3cad52ead735b54228
Premchand Hansda <premchandhansda006@gmail.com>
2024-07-17 20:24:54 +0000

    conversion and operation in Datatypes

---
+++ conversionInDatatypes.js
@@ -0,0 +1,64 @@
+let score=55
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)  // class based
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let roll = "815"
+
+let rollInNumber = Number(roll)
+console.log(typeof rollInNumber)
+console.log(rollInNumber)
+
+let marks = "90abc"
+
+let marksInNumber = Number(marks)
+console.log(typeof marksInNumber)
+console.log(marksInNumber)
+
+let points = null
+
+let pointsInNumber = Number(points)
+console.log(typeof pointsInNumber)
+console.log(pointsInNumber)
+
+let city
+
+let cityInNumber = Number(city)
+console.log(typeof cityInNumber)
+console.log(cityInNumber)
+
+let pass = true
+
+let passInNumber = Number(pass)
+console.log(typeof passInNumber)
+console.log(passInNumber)
+
+let name = "prem"
+
+let nameInNumber = Number(name)
+console.log(typeof nameInNumber)
+console.log(nameInNumber)
+
+let loggedIn = 1
+
+let loggedInBoolean = Boolean(loggedIn)
+console.log(typeof loggedInBoolean)
+console.log(loggedInBoolean)
+
+/* 
+1 => true
+0 => false
+"" => false
+"prem" => true
+*/
+
+let someNumber = 15
+
+let numInStr = String(someNumber)
+console.log(numInStr)
+console.log(typeof numInStr)
+
